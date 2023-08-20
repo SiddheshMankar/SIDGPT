256a2392a28ea2db5d8e4e6da5c14b6d72d4cc10
siddhesh mankar <siddheshmankar70@gmail.com>
2023-08-20 21:36:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const API_KEY="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"
+const submitbutton=document.querySelector("#sub");
+const OutputElement=document.querySelector('#output');
+const inputElement=document.querySelector('input');
+const historyElement=document.querySelector('.history')
+const btn=document.querySelector('button');
+function changeInput(value){
+  const inputElement = document.querySelector('input')
+  inputElement.value=value;
+}
+
+async function getmessage(){
+   console.log("ckicked");
+   const options={
+    method:'POST',
+    headers:{
+        'Authorization':`Bearer ${API_KEY}`,
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify({
+        model: "gpt-3.5-turbo",
+    messages: [
+     
+      {
+        role: "user",
+        content: inputElement.value
+      }
+    ],
+    max_tokens:100
+    })
+   }
+   try{
+     const response=   await fetch('https://api.openai.com/v1/chat/completions',options)
+        const data=await response.json()
+        console.log(data);
+        OutputElement.textContent=data.choices[0].message.content;
+        if(data.choices[0].message.content){
+            const pElement=document.createElement('p');
+            pElement.textContent=inputElement.value
+            pElement.addEventListener('click',()=>changeInput( pElement.textContent))
+            historyElement.append(pElement);
+        }
+
+
+    }
+    catch(error){  
+        console.error(error);
+    }
+}
+submitbutton.addEventListener('click',getmessage)
+
+function clearInput(){
+    inputElement.value='';
+}
+btn.addEventListener('click',clearInput);
